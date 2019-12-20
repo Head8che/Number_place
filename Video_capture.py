ad09a72473bfbb376cf5b9b7e02d5281ace192f8
Zubier Hagi <hagi@ualberta.ca>
2019-12-19 23:41:10 -0700

    Grid Detection has been implemented

--- Video_capture.py
+++ Video_capture.py
@@ -1,29 +1,73 @@
-import numpy as np 
 import cv2 as cv
+import numpy as np
 
 cap = cv.VideoCapture(0)
+apertureSize = 3
+ratio = 3
+threshold = 50
 
-#If nothing is captured
+#If nothing is captured; Camera isn't present or connected 
 if not cap.isOpened():
     print("Cannot open camera \n")
     print("Please Try again! ")
     exit()
+    
 
-#While  capture is read 
-while True:
-    ret,frame = cap.read()
-    #if frame is not read correctly ret is False
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
     if not ret:
         print("Error, cannot capture frame")
         break
-    #else frame has been read correctly ret is True
-    #Display the frame
-    gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
-    cv.imshow('Sudoku Solver', gray)
-    if cv.waitKey(1) == ord('q'):
-        break
-cap.release()
-cv.destroyAllWindows()
 
+    # Our operations on the frame come here
+    Sudoku = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+    edges = cv.Canny(Sudoku,threshold,threshold*ratio,apertureSize)
+    lines = cv.HoughLines(edges,1,np.pi/180,200)
 
     
+    if (lines is not None):
+        lines = sorted(lines, key=lambda line:line[0][0])
+        x = 0
+        y = 0
+        new_line = []
+        # position = []
+        for line in lines:
+            rho = line[0][0]
+            theta = line[0][1]
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            # x1 =(r * cos(theta) - 1000 * sin(theta))
+            x1 = int(x0 + 1000 * (-b))
+            # y1 = (r * sin(theta)+ 1000 * cos(theta))
+            y1 = int(y0 + 1000 * (a))
+            # x2 = (r * cos(theta)+ 1000 * sin(theta))
+            x2 = int(x0 - 1000 * (-b))
+            # y2 = (r * sin(theta)- 1000 * cos(theta))
+            y2 = int(y0 - 1000 * (a))
+            
+            #Angle checking > 45 == Veritcal Line
+            if(b > 0.5):
+                if(rho - x > 10):
+                    #Horizontal position update 
+                    x = rho
+                    cv.line(frame,(x1,y1),(x2,y2),(0,0,255),2)
+                    new_line.append([rho,theta,0])
+            #Horizontal Line 
+            else:
+                if (rho - y > 10):
+                    #Vertical position update
+                    y = rho
+                    cv.line(frame,(x1,y1),(x2,y2),(0,0,255),2)
+                    new_line.append([rho,theta,1])
+        
+    # Display the resulting frame
+    cv.imshow('Sudoku Solver', frame)   
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()

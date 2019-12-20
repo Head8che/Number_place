dbda37b847177a56cba65e6799fbef2e1c2048e6
Zubier Hagi <hagi@ualberta.ca>
2019-12-20 01:04:08 -0700

    Grid Detection update(HoughLinesP).

--- Video_capture.py
+++ Video_capture.py
@@ -3,8 +3,6 @@ import numpy as np
 
 cap = cv.VideoCapture(0)
 apertureSize = 3
-# ratio = 3
-# threshold = 90
 
 #If nothing is captured; Camera isn't present or connected 
 if not cap.isOpened():
@@ -12,7 +10,6 @@ if not cap.isOpened():
     print("Please Try again! ")
     exit()
     
-
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -24,7 +21,7 @@ while(True):
     Sudoku = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
     edges = cv.Canny(Sudoku,90,150,apertureSize)
     cv.imshow('Edges', edges)  
-    lines = cv.HoughLines(edges,1,np.pi/180,270)
+    lines = cv.HoughLinesP(edges,1,np.pi/180,270,100,10)
 
     
     if (lines is not None):
@@ -38,16 +35,8 @@ while(True):
             theta = line[0][1]
             a = np.cos(theta)
             b = np.sin(theta)
-            x0 = a * rho
-            y0 = b * rho
-            # x1 =(r * cos(theta) - 1000 * sin(theta))
-            x1 = int(x0 + 1000 * (-b))
-            # y1 = (r * sin(theta)+ 1000 * cos(theta))
-            y1 = int(y0 + 1000 * (a))
-            # x2 = (r * cos(theta)+ 1000 * sin(theta))
-            x2 = int(x0 - 1000 * (-b))
-            # y2 = (r * sin(theta)- 1000 * cos(theta))
-            y2 = int(y0 - 1000 * (a))
+            x1,y1,x2,y2 = line[0]
+            cv.line(frame,(x1,y1),(x2,y2),(0,0,255),2)
             
             #Angle checking > 45 == Veritcal Line
             if(b > 0.45):
